e8daf3778663dad6bd9ed24fb162a90345e15b24
Robert Boggs <r.boggs@guideline.com>
2019-09-17 17:32:44 -0700

    Add in memory sort

---
+++ src/sort.rb
@@ -0,0 +1,23 @@
+class Sort
+  # Only support 1 column sorting for now
+  def initialize(child, column)
+    @child = child
+    @schema = %w[movieId title genres]
+    @column_index = @schema.index(column)
+    @unsorted_list = []
+    @sorted_list = nil
+  end
+
+  def next
+    while(value = @child.next)
+      @unsorted_list << value
+    end
+
+    if @sorted_list.nil?
+      @sorted_list = @unsorted_list.sort_by { |record| record[@column_index] }
+    end
+
+    # TODO: use more efficient data structure
+    @sorted_list.length.zero? ? nil : @sorted_list.shift
+  end
+end
