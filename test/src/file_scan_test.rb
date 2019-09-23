6d1ba1d71a22bb5e4bebd11d071389bb759e169b
Robert Boggs <r.boggs@guideline.com>
2019-09-22 20:48:19 -0700

    Add test for file scan reset

--- test/src/file_scan_test.rb
+++ test/src/file_scan_test.rb
@@ -16,18 +16,36 @@ module Src
     end
 
     def test_can_scan_values
-      scan = ::FileScan.new(table: "movies")
-      assert_equal [1, "Toy Story (1995)", "Adventure|Animation|Children|Comedy|Fantasy"], scan.next
-      assert_equal [2, "Jumanji (1995)", "Adventure|Children|Fantasy"], scan.next
-      assert_equal [3, "Grumpier Old Men (1995)", "Comedy|Romance"], scan.next
-      assert_equal [4, "Waiting to Exhale (1995)", "Comedy|Drama|Romance"], scan.next
-      assert_equal [5, "Father of the Bride Part II (1995)", "Comedy"], scan.next
-      assert_equal [6, "Heat (1995)", "Action|Crime|Thriller"], scan.next
-      assert_equal [7, "Sabrina (1995)", "Comedy|Romance"], scan.next
-      assert_equal [8, "Toy Story (1995)", "Adventure|Animation|Children|Comedy|Fantasy"], scan.next
-      assert_equal [9, "Sudden Death (1995)", "Action"], scan.next
-      assert_equal [10, "GoldenEye (1995)", "Action|Adventure|Thriller"], scan.next
-      assert_nil scan.next
+      @scan = ::FileScan.new(table: "movies")
+
+      check_scan
+    end
+
+    def test_can_reset
+      @scan = ::FileScan.new(table: "movies")
+
+      assert_equal [1, "Toy Story (1995)", "Adventure|Animation|Children|Comedy|Fantasy"], @scan.next
+      assert_equal [2, "Jumanji (1995)", "Adventure|Children|Fantasy"], @scan.next
+      assert_equal [3, "Grumpier Old Men (1995)", "Comedy|Romance"], @scan.next
+
+      @scan.reset
+      check_scan
+    end
+
+    private
+
+    def check_scan
+      assert_equal [1, "Toy Story (1995)", "Adventure|Animation|Children|Comedy|Fantasy"], @scan.next
+      assert_equal [2, "Jumanji (1995)", "Adventure|Children|Fantasy"], @scan.next
+      assert_equal [3, "Grumpier Old Men (1995)", "Comedy|Romance"], @scan.next
+      assert_equal [4, "Waiting to Exhale (1995)", "Comedy|Drama|Romance"], @scan.next
+      assert_equal [5, "Father of the Bride Part II (1995)", "Comedy"], @scan.next
+      assert_equal [6, "Heat (1995)", "Action|Crime|Thriller"], @scan.next
+      assert_equal [7, "Sabrina (1995)", "Comedy|Romance"], @scan.next
+      assert_equal [8, "Toy Story (1995)", "Adventure|Animation|Children|Comedy|Fantasy"], @scan.next
+      assert_equal [9, "Sudden Death (1995)", "Action"], @scan.next
+      assert_equal [10, "GoldenEye (1995)", "Action|Adventure|Thriller"], @scan.next
+      assert_nil @scan.next
     end
   end
 end
